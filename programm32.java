4cb4b2994205ca6d9d59ed378b94783099a7dfe3
pruthviraj-raj <pruthvirajdubal66@gmail.com>
2026-07-26 18:43:29 +0530

    Add files via upload

---
+++ programm32.java
@@ -0,0 +1,13 @@
+public class programm32 {
+    /*
+    PRINT ASCII 
+    VALUE 
+    */
+   public static void main(String[] args) {
+    
+    for(char ch='A';ch<='Z';ch++) {
+        System.out.println(ch+"="+(int)ch);
+    }
+   }
+    
+}
---
+++ programm34.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class programm34 {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+
+        int base=sc.nextInt();
+        int exp=sc.nextInt();
+        int result=1;
+        
+        
+        for(int i=1;i<=exp;i++){
+            result=result*base;
+        }
+        System.out.print("Result="+result);
+    }
+    
+}
